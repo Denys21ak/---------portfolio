3de0c9e0c5960316686e4c4b64ef88f21abb60e4
Denis Shevchenko <80395255+Denys21ak@users.noreply.github.com>
2023-02-05 00:01:40 +0200

    Add files via upload

--- my.js
+++ my.js
@@ -1,6 +1,28 @@
-$(function(){
-  $(".bt1").click(function(){
-      $("h3").html("FKJ")
-      $("h3").css({color:'green'})
-  });
-});
+let h1 = 2;
+if(h1>10){
+let result = 'good';
+} else {
+  result = 'ok';
+}
+console.log(result + 'Using if');
+
+
+document.forms.publish.onsubmit = function() {
+  var num1 = this.num1.value;
+  var num2 = this.num2.value;
+  if(num1>num2){
+    alert('Число '+ num1 + ' Більше за число ' + num2)
+  }else{
+    alert('Число '+ num1 + ' Менше за число ' +num2)
+  }
+  return false;
+};
+
+
+
+document.getElementById('close').onclick = function() { 
+  document.getElementById('bt2').style.display = "none";
+  document.getElementById('bt').style.display = "none";
+}
+
+
