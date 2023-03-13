d2a2c253bfccd48019e2bf2d44a833773d327749
Denis Shevchenko <80395255+Denys21ak@users.noreply.github.com>
2023-03-13 18:33:55 +0200

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php 
 // if (!empty($_POST['name']) AND !empty($_POST['phone']) ){
   
@@ -42,4 +43,50 @@ function clear_data($val){
 if (isset($_POST['submit'])){
   mail($to, $subject, $message, $headers);
 }
+=======
+<?php 
+// if (!empty($_POST['name']) AND !empty($_POST['phone']) ){
+  
+//   $headers = 'From: Шевченко Денис\r\n'.
+//              'Reply-To: sevcenkodenis21gmail.com\r\n'.
+//               'X-Mailer: PHP/'. phpversion();
+             
+//   $theme = 'Новое сообщение';
+
+//   $letter = 'Данные сообщения:\r\n';
+//   $letter .='Имя: '.$_POST['name'].'\r\n';
+//   $letter .='Телефон: '.$_POST['phone'].'\r\n';
+  
+//   if (mail('sevcenkodenis21gmail.com', $theme, $letter, $headers)){
+//     echo "Сообщение отправлено!";
+//   } else {
+//     echo "Ошибка при отправке!";
+//   }
+// } else {
+//   echo "Ошибка: Заполните все поля!";
+// }
+
+$to = 'sevcenkodenis21gmail.com';
+$name = clear_data($_POST['name']);
+$email = clear_data($_POST['email']);
+$text = clear_data($_POST['text']);
+$subject = 'Заявка з сайта';
+$message = 'Повідомлення';
+mail($to, $subject, $message, $headers);
+$headers = "From: sevcenkodenis21gmail.com\r\n".
+             "Reply-To: sevcenkodenis21gmail.com\r\n".
+              "X-Mailer: PHP/". phpversion();
+
+$message = 'Имя ' . $name. "\n" . 'Email ' . $email. "\n" . 'Massage ' . $text. "\n";
+function clear_data($val){
+  $val = trim($val);
+  $val = stripcslashes($val);
+  $val = htmlspecialchars($val);
+  return $vel;
+}
+
+if (isset($_POST['submit'])){
+  mail($to, $subject, $message, $headers);
+}
+>>>>>>> 9477b7988e891a2407136b67f96aab34c18ab718
 ?>
